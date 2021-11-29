4e656b244c929371ad05b0229bf3d220e42e6669
katemayorova <katemayorova0@gmail.com>
2021-11-29 21:12:35 +0300

    Implement getting photo names and links

--- vk_api.py
+++ vk_api.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 
 
@@ -10,10 +12,29 @@ class VkApi:
         self.token = token
 
     def get_photos(self, owner_id: str):
+        response_json = self.__get_from_api()
+        parsed_photo = self.__parse_photos(response_json)
+
+        return parsed_photo
+
+    @staticmethod
+    def __parse_photos(response_json):
+        photos = response_json['response']['items']
+        parsed_photo = {}
+        for photo in photos:
+            name = f'{photo["likes"]["count"]}.jpg'
+            if name in parsed_photo:
+                date = datetime.utcfromtimestamp(photo['date']).strftime('%Y%m%d')
+                name = f'{photo["likes"]["count"]}_{date}.jpg'
+            url = max(photo['sizes'], key=lambda key: key['height'])['url']
+            parsed_photo[name] = url
+        return parsed_photo
+
+    def __get_from_api(self):
         photos_url = self.URL + 'photos.get'
         photos_params = {
             'count': 1000,
-            'extended': 0,
+            'extended': 1,
             'access_token': self.token,
             'v': self.API_VERSION,
             'album_id': 'profile'
