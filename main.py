68f269d8dc5fdb9de1b2eacf8e88ec2c659ca47a
katemayorova <katemayorova0@gmail.com>
2021-12-08 19:34:33 +0300

    Implement upload to YD

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from vk_api import VkApi
 import os
+
+from vk_api import VkApi
+from ya_api import YaApi
 
 if __name__ == '__main__':
     vk_owner_id = "begemot_korovin"
     vk_token = os.getenv("VK_TOKEN")
     vk_api = VkApi(vk_token)
-    photo_response = vk_api.get_photos(vk_owner_id)
-    print(photo_response)
+    photo_count = 5
+    parsed_photos = vk_api.get_photos(vk_owner_id, photo_count)
+
+    ya_token = os.getenv("YA_TOKEN")
+    ya_api = YaApi(ya_token)
+    ya_api.get_and_upload("photos", parsed_photos)
