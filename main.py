a015126013c718e8cd991ee571ebdb4ebecbbb65
katemayorova <katemayorova0@gmail.com>
2021-12-08 19:42:07 +0300

    Rename vk_screen_name variable

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from vk_api import VkApi
 from ya_api import YaApi
 
 if __name__ == '__main__':
-    vk_owner_id = "begemot_korovin"
+    vk_screen_name = "begemot_korovin"
     vk_token = os.getenv("VK_TOKEN")
     vk_api = VkApi(vk_token)
     photo_count = 5
-    parsed_photos = vk_api.get_photos(vk_owner_id, photo_count)
+    parsed_photos = vk_api.get_photos(vk_screen_name, photo_count)
 
     ya_token = os.getenv("YA_TOKEN")
     ya_api = YaApi(ya_token)
