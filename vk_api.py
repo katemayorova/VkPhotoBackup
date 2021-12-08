a851521be4210eaa4206de4f9fcb522d61cf020d
katemayorova <katemayorova0@gmail.com>
2021-12-08 19:29:31 +0300

    Add count, vk_screen_name parameters, reversed sort order

--- vk_api.py
+++ vk_api.py
@@ -11,11 +11,23 @@ class VkApi:
     def __init__(self, token):
         self.token = token
 
-    def get_photos(self, owner_id: str):
-        response_json = self.__get_from_api()
-        parsed_photo = self.__parse_photos(response_json)
+    def get_photos(self, vk_screen_name: str, count: int = 1000):
+        vk_owner_id = self.__get_user_id(vk_screen_name)
+        response_json = self.__get_from_api(vk_owner_id, count)
+        parsed_photos = self.__parse_photos(response_json)
 
-        return parsed_photo
+        return parsed_photos
+
+    def __get_user_id(self, screen_name):
+        utils_url = self.URL + 'utils.resolveScreenName'
+        utils_params = {
+            'access_token': self.token,
+            'v': self.API_VERSION,
+            'screen_name': screen_name
+        }
+        response = requests.get(utils_url, params=utils_params)
+        response_json = response.json()
+        return response_json['response']['object_id']
 
     @staticmethod
     def __parse_photos(response_json):
@@ -30,14 +42,16 @@ class VkApi:
             parsed_photo[name] = url
         return parsed_photo
 
-    def __get_from_api(self):
+    def __get_from_api(self, vk_owner_id: str, count: int):
         photos_url = self.URL + 'photos.get'
         photos_params = {
-            'count': 1000,
+            'count': count,
             'extended': 1,
             'access_token': self.token,
             'v': self.API_VERSION,
-            'album_id': 'profile'
+            'album_id': 'profile',
+            'owner_id': vk_owner_id,
+            'rev': 1
         }
         response = requests.get(photos_url, params=photos_params)
         return response.json()
